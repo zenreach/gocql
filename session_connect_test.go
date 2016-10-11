b10d89e95533c0ee076759381c5dd3844ec28fe8
Justin "Gus" Knowlden <gus@gusg.us>
2016-10-11 11:03:30 -0400

    cleaning up test code for session connection

--- session_connect_test.go
+++ session_connect_test.go
@@ -1,60 +1,68 @@
 package gocql
 
 import (
-	"testing"
-	"net"
-	"time"
 	"log"
+	"net"
+	"testing"
+	"time"
 )
 
-func TestSession_connect_WithNoTranslator(t *testing.T) {
+func setupTestTcpServer(t *testing.T) (net.Listener, net.IP, int) {
 	srvr, err := net.Listen("tcp", ":0")
 	assertNil(t, "error when creating tcp server", err)
-	defer srvr.Close()
-
 	testHost, testPort, err := parseIPPort(srvr.Addr().String())
 	log.Printf("using host=%s and port=%d for test tcp server (err=%v)", testHost.String(), testPort, err)
+	return srvr, testHost, testPort
+}
+
+func tcpConnWaitAndClose(t *testing.T, srvr net.Listener, tchan chan<- error) {
+	conn, err := srvr.Accept()
+	if conn != nil {
+		log.Printf("connection received from addr=%+v", conn.RemoteAddr())
+		conn.Close()
+	}
+	tchan <- err
+}
+
+func testConnErrorHandler(t *testing.T) ConnErrorHandler {
+	return connErrorHandlerFn(func(conn *Conn, err error, closed bool) {
+		t.Errorf("in connection handler: %v", err)
+	})
+}
+
+func assertConnectionEventually(t *testing.T, delay time.Duration, tchan <-chan error) {
+	select {
+	case err := <-tchan:
+		if err != nil {
+			t.Errorf("accepting connection: %v", err)
+		}
+	case <-time.After(delay):
+		t.Error("timed out waiting for connection")
+	}
+}
+
+func TestSession_connect_WithNoTranslator(t *testing.T) {
+	srvr, testHost, testPort := setupTestTcpServer(t)
+	defer srvr.Close()
 
 	session := createTestSession()
 	defer session.Close()
 
 	tchan := make(chan error)
 	defer close(tchan)
-	go func(t *testing.T, srvr net.Listener, cchan chan<- error) {
-		conn, err := srvr.Accept()
-		if conn != nil {
-			log.Printf("connection received from addr=%+v", conn.RemoteAddr())
-			conn.Close()
-		}
-		cchan <- err
-	}(t, srvr, tchan)
+	go tcpConnWaitAndClose(t, srvr, tchan)
 
-	errHandler := connErrorHandlerFn(func (conn *Conn, err error, closed bool) {
-		t.Errorf("in connection handler: %v", err)
-	})
-
-	session.connect(srvr.Addr().String(), errHandler, &HostInfo{
+	session.connect(srvr.Addr().String(), testConnErrorHandler(t), &HostInfo{
 		peer: testHost.String(),
 		port: testPort,
 	})
 
-	select {
-	case err := <-tchan:
-		if err != nil {
-			t.Errorf("accepting connection: %v", err)
-		}
-	case <-time.After(100*time.Millisecond):
-		t.Error("timed out waiting for connection")
-	}
+	assertConnectionEventually(t, 100*time.Millisecond, tchan)
 }
 
 func TestSession_connect_WithTranslator(t *testing.T) {
-	srvr, err := net.Listen("tcp", ":0")
-	assertNil(t, "error when creating tcp server", err)
+	srvr, testHost, testPort := setupTestTcpServer(t)
 	defer srvr.Close()
-
-	testHost, testPort, err := parseIPPort(srvr.Addr().String())
-	log.Printf("using host=%s and port=%d for test tcp server (err=%v)", testHost.String(), testPort, err)
 
 	session := createTestSession()
 	defer session.Close()
@@ -62,30 +70,13 @@ func TestSession_connect_WithTranslator(t *testing.T) {
 
 	tchan := make(chan error)
 	defer close(tchan)
-	go func(t *testing.T, srvr net.Listener, cchan chan<- error) {
-		conn, err := srvr.Accept()
-		if conn != nil {
-			log.Printf("connection received from addr=%+v", conn.RemoteAddr())
-			conn.Close()
-		}
-		cchan <- err
-	}(t, srvr, tchan)
+	go tcpConnWaitAndClose(t, srvr, tchan)
 
-	errHandler := connErrorHandlerFn(func (conn *Conn, err error, closed bool) {
-		t.Errorf("in connection handler: %v", err)
-	})
-
-	session.connect("10.10.10.10:5432", errHandler, &HostInfo{
+	// the provided address will be translated
+	session.connect("10.10.10.10:5432", testConnErrorHandler(t), &HostInfo{
 		peer: "10.10.10.10",
 		port: 5432,
 	})
 
-	select {
-	case err := <-tchan:
-		if err != nil {
-			t.Errorf("accepting connection: %v", err)
-		}
-	case <-time.After(100*time.Millisecond):
-		t.Error("timed out waiting for connection")
-	}
+	assertConnectionEventually(t, 100*time.Millisecond, tchan)
 }
