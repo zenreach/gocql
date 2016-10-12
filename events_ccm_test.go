9ecc3c19481b7b0792e737b59400defd62f05a01
Ryan Bourgeois <bluedragonx@gmail.com>
2016-10-11 22:06:02 -0700

    Import packages from fork.

--- events_ccm_test.go
+++ events_ccm_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gocql/gocql/internal/ccm"
+	"github.com/zenreach/gocql/internal/ccm"
 )
 
 func TestEventDiscovery(t *testing.T) {
--- token.go
+++ token.go
@@ -13,7 +13,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/gocql/gocql/internal/murmur"
+	"github.com/zenreach/gocql/internal/murmur"
 )
 
 // a token partitioner
